0cd77b87383133e1b515f5cfa6eb6bf07a8cee34
gfdelrosario12 <gladelrosario12@gmail.com>
2026-05-03 14:17:58 +0800

    fix: fixed api url bases of frontend

--- src/api/workerProfile.ts
+++ src/api/workerProfile.ts
@@ -1,6 +1,6 @@
 import { authenticatedFetch } from './fetch';
 
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'http://siteguardph.duckdns.org/api';
 
 export interface WorkerFullUpdateDTO {
   name?: string;
